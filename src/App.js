1808482184c40695adcaabb5ef2ac44d81889334
subekshyakharel <147831342+subekshyakharel@users.noreply.github.com>
2024-06-10 17:36:35 +0545

    fix quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Quizdata } from './data/Quizdata';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [currentQsn, nextQsn] = useState(0)
+
+  const handleNextQsn = () =>{
+    if(currentQsn< Quizdata.length-1){
+    nextQsn(currentQsn + 1)
+    } else {
+      alert("You have reched the end.")
+    }
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="wrapper">
+      <div className="quiz">
+
+        <div className="qsn">
+          <span className='span'>{Quizdata[currentQsn].id}.</span>
+          <span>{Quizdata[currentQsn].Question}</span>
+        </div>
+         
+         
+        <div className='contain-option'>
+        <h4>options</h4>
+
+        {Quizdata[currentQsn].option.map((option, i)=>{
+         return (<div key={i} className="option">
+          <span>{i+1}. </span>
+          <span>{option}</span>
+        </div>
+         )
+        })}
+
+        
+
+        </div>
+        <div className='contain-btn'>
+        <button className='btn' onClick={handleNextQsn}>Next</button>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/data/Quizdata.js
@@ -0,0 +1,26 @@
+export const Quizdata = [
+    {
+    id :1,
+    Question : "Which of the following is not a JavaScript data type?",
+    option : ["String",  "Number", "Boolean", "Float"], 
+    answer : 4
+    }, 
+    {
+        id:2, 
+        Question : "How can you add a comment in JavaScript?", 
+        option : ["<!-- This is a comment -->", "# This is a comment", "// This is a comment", "/* This is a comment */" ], 
+        answer: 3
+    }, 
+    {
+        id :3, 
+        Question : "Which of the following is used to declare a variable in JavaScript?", 
+        option : ["var", "let", "const", "All of the above" ], 
+        answer : 4
+    }, 
+    {
+        id : 4, 
+        Question : "Which operator is used to assign a value to a variable in JavaScript?", 
+        option : ["*", "-", "=", "/"], 
+        answer : 3
+    }
+]
